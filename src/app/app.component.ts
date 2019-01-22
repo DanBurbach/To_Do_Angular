0229b3c7f35d30ff2ef5e827fe5efc94e1927e8c
Daniel Burbach and Jared Reando <daniel@>
2019-01-22 15:08:00 -0800

    initial angular commit for To_Do List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'geronimo',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'to-do';
+  title = 'your app!';
+  currentFocus: string = 'Angular Homework!';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+  tasks: Task[] = [
+    new Task('Finish coding homework'),
+    new Task('Count hours left in day'),
+    new Task('No hours left. Sleep and repeat')
+  ];
 }
